c9e7e58a9b60a45a3e5e4cad555f8152c8da7496
GaneshMane34 <ganeshmane2009@gmail.com>
2020-12-28 21:23:22 +0530

    add student

---
+++ src/main/java/com/codenuclear/Student.java
@@ -0,0 +1,29 @@
+ package com.codenuclear;
+
+public class Student 
+{
+	String stuName;
+	String stuCity;
+	int stuId;
+	
+	public String getStuName() {
+		return stuName;
+	}
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+	
+	public String getStuCity() {
+		return stuCity;
+	}
+	public void setStuCity(String stuCity) {
+		this.stuCity = stuCity;
+	}
+	
+	public int getStuId() {
+		return stuId;
+	}
+	public void setStuId(int stuId) {
+		this.stuId = stuId;
+	}
+}
